faabed46a47b15d7b0d6e7b68f6b2167577236c4
Daniel Amorim <amorimdaniel0906@gmail.com>
2022-09-12 16:49:08 -0300

    Add files via upload

---
+++ Exercicio-Aula05/src/java/api/DadosServletXML.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.*;
+
+/**
+ *
+ * @author Fatec
+ */
+@WebServlet(name = "DadosServletXML", urlPatterns = {"/daniel-dados.xml"})
+public class DadosServletXML extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/xml;charset=UTF-8");
+        response.getWriter().println("<?xml version ='1.0' encoding='UTF-8' ?>");
+        response.getWriter().println("<dados>");
+        response.getWriter().println("<dado ra='1290482222017' nome='Daniel Amorim de Castro'>Aluno</dado> ");
+        response.getWriter().println("<disciplinas type='array'>");
+        response.getWriter().println("<value>Banco de Dados</value>");
+        response.getWriter().println("<value>POO</value>");
+        response.getWriter().println("<value>LPIV</value>");
+        response.getWriter().println("<value>SOII</value>");
+        response.getWriter().println("<value>Metodologia</value>");
+        response.getWriter().println("</disciplinas>");
+        response.getWriter().println("</dados>");
+                
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
